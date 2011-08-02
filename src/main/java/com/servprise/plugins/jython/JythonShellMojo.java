0948c4f502e1fa9207b1829e55885ee0cc82f53b
nigel <nigel@Nigels-Macbook.local>
2011-08-02 16:37:52 +1000

    Create the shell with the classpath provided by Maven

--- src/main/java/com/servprise/plugins/jython/JythonShellMojo.java
+++ src/main/java/com/servprise/plugins/jython/JythonShellMojo.java
@@ -16,9 +16,29 @@
 
 package com.servprise.plugins.jython;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.python.core.Options;
+import org.python.core.Py;
+import org.python.core.PyException;
+import org.python.core.PyModule;
+import org.python.core.PyObject;
+import org.python.core.PyString;
+import org.python.core.PySystemState;
+import org.python.core.imp;
+import org.python.util.InteractiveConsole;
 import org.python.util.jython;
 
 /**
@@ -28,11 +48,65 @@ import org.python.util.jython;
  *
  * @prefix jython
  * @goal shell
+ * @requiresDependencyResolution runtime
  */
 public class JythonShellMojo extends AbstractMojo
 {
+    /**
+     * @parameter default-value="${project.compileClasspathElements}"
+     * @required
+     * @readonly
+     */
+    private List<String> dependency;
+    
     public void execute() throws MojoExecutionException, MojoFailureException
     {
-        jython.main(new String[]{});
+        // Setup the basic python system state from these options
+        PySystemState.initialize(PySystemState.getBaseProperties(),
+                                 new Properties(), new String[0]);
+
+        // Now create an interpreter
+        InteractiveConsole interp = null;
+        try {
+            String interpClass = PySystemState.registry.getProperty(
+                                    "python.console",
+                                    "org.python.util.InteractiveConsole");
+            interp = (InteractiveConsole)
+                             Class.forName(interpClass).newInstance();
+        } catch (Exception e) {
+            interp = new InteractiveConsole();
+        }
+
+        //System.err.println("interp");
+        PyModule mod = imp.addModule("__main__");
+        interp.setLocals(mod.__dict__);
+        //System.err.println("imp");
+
+        String msg = "";
+        if (Options.importSite) {
+            try {
+                imp.load("site");
+            } catch (PyException pye) {
+                if (!Py.matchException(pye, Py.ImportError)) {
+                    System.err.println("error importing site");
+                    Py.printException(pye);
+                    System.exit(-1);
+                }
+            }
+        }
+
+
+        Py.getSystemState().path.insert(0, new PyString(""));
+        for (String a: dependency) {
+    		Py.getSystemState().path.insert(0, new PyString(a));
+    	}
+
+        try {
+            interp.interact();
+        } catch (Throwable t) {
+            Py.printException(t);
+        }
+        interp.cleanup();
+//        System.exit(0);
     }
 }
